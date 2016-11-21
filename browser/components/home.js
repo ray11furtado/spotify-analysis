620689c075f518a3d8b4e6068fab04e249ffb2a6
Ray Furtado <rayfurtado11@gmail.com>
2016-11-21 15:38:27 -0500

    playlists now its own component

--- browser/components/home.js
+++ browser/components/home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import Playlists from '../containers/playlists';
 import * as actions from '../actions';
 
 require('../style/home.scss');
@@ -13,39 +12,11 @@ class Home extends Component {
 		this.props.signin();
 	}
 
-	displayPlaylist() {
-		if (this.props.user.playlist[0]) {
-			const allPlaylist = this.props.user.playlist;
-			return allPlaylist.map(singlePlaylist =>
-				<div key={singlePlaylist.snapshot_id} className="col-md-3 playlist-container">
-					<div className="container">
-						<h5><strong>{singlePlaylist.name}</strong></h5>
-						<img
-							src={singlePlaylist.images[0] ? singlePlaylist.images[0].url : 'https://image.freepik.com/free-icon/question-mark_318-52837.jpg'}
-							alt="Playlist"
-							className="playlist-img"
-						/>
-					</div>
-				</div>,
-			);
-		} return <div className="text-center loading">Loading...</div>;
-	}
 
-	header() {
-		if (this.props.user.playlist[0]) {
-			return <div className="playlist-header text-center">Your Playlist</div>;
-		} return <div />;
-	}
 
 	render() {
 		return (
-			<div className="home container">
-					<h2>{this.header()}</h2>
-				<ul>
-					{this.displayPlaylist()}
-					{this.props.children}
-				</ul>
-			</div>
+			<div>Home Page</div>
 			);
 	}
 }
--- browser/containers/playlists.js
+++ browser/containers/playlists.js
@@ -2,4 +2,46 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-export default () => <div>Hello</div>;
+require('../style/playlist.scss');
+
+class Playlists extends Component {
+
+	header() {
+		if (this.props.user.playlist[0]) {
+			return <div className="playlist-header text-center">Your Playlist</div>;
+		} return <div />;
+	}
+
+	displayPlaylist() {
+		if (this.props.user.playlist[0]) {
+			const allPlaylist = this.props.user.playlist;
+			return allPlaylist.map(singlePlaylist =>
+				<div key={singlePlaylist.snapshot_id} className="col-md-3 playlist-container">
+					<div className="container">
+						<h5><strong>{singlePlaylist.name}</strong></h5>
+						<img
+							src={singlePlaylist.images[0] ? singlePlaylist.images[0].url : 'https://image.freepik.com/free-icon/question-mark_318-52837.jpg'}
+							alt="Playlist"
+							className="playlist-img"
+						/>
+					</div>
+				</div>,
+			);
+		} return <div className="text-center loading">Loading Your Playlists </div>;
+	}
+	render() {
+		return (
+			<div className="home container">
+				<h2>{this.header()}</h2>
+				<ul>
+					{this.displayPlaylist()}
+				</ul>
+			</div>
+			);
+	}
+}
+
+function mapStateToProps(state) {
+	return { user: state.user };
+}
+export default connect(mapStateToProps, actions)(Playlists);
