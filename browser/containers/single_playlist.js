7647b1ef672b01e8c166810a862171a163249099
Ray Furtado <rayfurtado11@gmail.com>
2016-11-21 20:48:11 -0500

    displaying first 100 tracks after playlist is selected

--- browser/containers/single_playlist.js
+++ browser/containers/single_playlist.js
@@ -3,17 +3,26 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class singePlaylist extends Component {
-
-	display() {
-		console.log(this.props.search);
-		if (this.props.search) {
-			return <div>playlist</div>;
-		} return <div>Loading...</div>;
+	displayTitle() {
+		if (this.props.search.content) {
+			return <h2>{this.props.search.content.name}</h2>;
+		} return <h2>Loading Playlist...</h2>;
 	}
+
+	displayTracks() {
+		if (this.props.search.tracks) {
+			const songs = this.props.search.tracks.items;
+			return songs.map(song =>
+				<div>{song.track.name}, {song.track.artists[0].name}</div>,
+			);
+		} return <div>Loading Tracks...</div>;
+	}
+
 	render() {
 		return (
 			<div>
-				{this.display()}
+				{this.displayTitle()}
+				{this.displayTracks()}
 			</div>
 			);
 	}
--- browser/reducers/searched_content_reducer.js
+++ browser/reducers/searched_content_reducer.js
@@ -7,8 +7,6 @@ const INITIAL_STATE = { type: null, content: null, tracks: null };
 export default function (state = INITIAL_STATE, action) {
 	switch (action.type) {
 		case SEARCH_PLAYLIST:
-		// console.log('form reducer content:', action.payload.content);
-		// console.log('form reducer tracks:', action.payload.tracks);
 			return { ...state,
 				type: action.payload.type,
 				content: action.payload.content,
