92c67ce89737d8349214f64cd90ec9272894339f
Ray Furtado <rayfurtado11@gmail.com>
2016-11-29 18:48:46 -0500

    started song analysis container

--- browser/containers/song_analysis.jsx
+++ browser/containers/song_analysis.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import Loading from '../components/loading';
 
 class songAnalysis extends Component {
 
   componentWillMount() {
     this.props.analyzeLyrics(this.props.songInfo.lyrics);
   }
+
+  loading() {
+    if (!this.props.song.lyrics) {
+      return (
+        <div>
+          <h3>Fetching Lyrics</h3>
+          <Loading />
+        </div>
+      );
+    }
+    return <h2>Done</h2>;
+  }
+
+  render() {
+    return (
+      <div>{this.loading()}</div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-  return { songInfo: state.search,
-            songAnalysis: state.analysis };
+  return { song: state.singleSongAnalysis };
 }
 
 export default connect(mapStateToProps, actions)(songAnalysis);
