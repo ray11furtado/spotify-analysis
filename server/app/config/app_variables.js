276821ae0d069570ad167e8b7b2b517f476b8669
Ray Furtado <rayfurtado11@gmail.com>
2016-12-09 16:38:13 -0500

    favicon

--- server/app/config/app_variables.js
+++ server/app/config/app_variables.js
@@ -4,6 +4,7 @@ import util from 'util';
 
 const rootPath = path.join(__dirname, '../../../');
 const indexPath = path.join(rootPath, './server/app/views/index.html');
+const faviconPath = path.join(rootPath, './server/app/views/favicon.ico');
 
 
 const env = require(path.join(rootPath, './server/env'));
@@ -21,4 +22,5 @@ export default (app) => {
 	app.setValue('projectRoot', rootPath);
 	app.setValue('indexHTMLPath', indexPath);
 	app.setValue('log', logMiddleWare);
+	app.setValue('faviconPath', faviconPath);
 };
